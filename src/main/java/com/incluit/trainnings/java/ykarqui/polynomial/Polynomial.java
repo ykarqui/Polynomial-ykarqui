cf2840389d151db80a4436d5ccab1898f52a931e
ykarqui3 <ykarqui107@alumnos.iua.edu.ar>
2020-05-27 03:26:54 -0300

    checked all methods

--- src/main/java/com/incluit/trainnings/java/ykarqui/polynomial/Polynomial.java
+++ src/main/java/com/incluit/trainnings/java/ykarqui/polynomial/Polynomial.java
@@ -7,37 +7,6 @@ public class Polynomial {
     Nodo root;
     int size;
 
-    public void add() {
-        System.out.println("LLega a add");
-        Nodo n = new Nodo(0,0);
-        if (root == null) {
-            root = n;
-        } else {
-            Nodo p = root;
-            while (p.next != null) {
-                p = p.next;
-            }
-            p.next = n;
-        }
-        size ++;
-    }
-
-    public void addCoef(int coef) {
-        if (coef != 0) {
-            Nodo n = new Nodo(coef,0);
-            if (root == null) {
-                root = n;
-            } else {
-                Nodo p = root;
-                while (p.next != null) {
-                    p = p.next;
-                }
-            p.next = n;
-        }
-        size ++;
-        }
-    }
-
     // 1. Polynomial(): ​ 
     //     este constructor debe crear un objeto ​ Polynomial ​ igual al polinomio 0(cero) (grado n = 0).
     public Polynomial(){
@@ -52,6 +21,9 @@ public class Polynomial {
             p.next = n;
         }
         size ++;
+        System.out.println("EXPONENTES: " + n.exponent);
+        System.out.println("COEFFS: " + n.coefficient);
+            
     }
 
     // 2. Polynomial(int coef[]): ​ 
@@ -61,20 +33,17 @@ public class Polynomial {
     //      entonces el término correspondiente en el polinomio no existe y ​ no debe agregarse ​ .
     public Polynomial(int coef []) {
         for (int i = 0; i < coef.length; i++){
-            if (coef[i] != 0) {
-                Nodo n = new Nodo(coef[i], i);
-                size ++;
-                if (root == null) {
-                    root = n;
-                } else {
-                    Nodo p = root;
-                    while (p.next != null) {
-                        p = p.next;
-                    }
-                p.next = n;
+            Nodo n = new Nodo(coef[i], i+1);
+            size ++;
+            if (root == null) {
+                root = n;
+            } else {
+                Nodo p = root;
+                while (p.next != null) {
+                    p = p.next;
                 }
-            
-        }
+            p.next = n;
+            }
         }
     }
 
@@ -82,41 +51,48 @@ public class Polynomial {
     //     retorna un Polynomial igual a la suma entre ​this ​y pol
     public Polynomial add(Polynomial pol){
         
-        Polynomial current = this;  // actual 
         Polynomial result = new Polynomial();   // parameter
         
-        Nodo x = current.root.next; // actual
+        Nodo x = this.root.next; // actual
+        System.out.println("x= " + x.coefficient + "x^" + x.exponent);
         Nodo y = pol.root.next; // parameter
-        
+        System.out.println("y= " + y.coefficient + "x^" + y.exponent);
+        int calculatedCoeff = 0;
         while (x != null || y != null) {
             Nodo t = null;
             
             if(x == null){ 
                 t = new Nodo(y.coefficient, y.exponent);  
                 y = y.next; 
+                calculatedCoeff = y.exponent;
             }
             else if (y == null) { 
                 t = new Nodo(x.coefficient, x.exponent);  
                 x = x.next; 
+                calculatedCoeff = x.exponent;
             }
-            else if (x.exponent > y.exponent) { 
+            else if (x.exponent < y.exponent) { 
                 t = new Nodo(x.coefficient, x.exponent);  
                 x = x.next; 
+                calculatedCoeff = x.exponent;
             } 
-            else if (x.exponent < y.exponent) { 
+            else if (x.exponent > y.exponent) { 
                 t = new Nodo(y.coefficient, y.exponent);  
                 y = y.next; 
+                calculatedCoeff = y.exponent;
             } 
             else {
                 int coef = x.coefficient + y.coefficient;
                 int exp  = x.exponent;
+                System.out.println("Result = " + coef + "x^" + exp);
                 x = x.next;
                 y = y.next;
-                if (coef == 0) continue;
+                calculatedCoeff = coef;
                 t = new Nodo(coef, exp);
             }
-        
-            result.root.next = t;
+            System.out.println(calculatedCoeff);
+            result.setCoefficient(result.size, calculatedCoeff);
+            // result.root.next = t;
             result.root = result.root.next;
         }
         return result;
@@ -125,14 +101,12 @@ public class Polynomial {
     // 4. int getCoefficient(int x): ​
     //     Devuelve el valor del coeficiente del grado ​ x
     public int getCoefficient(int x) {
-        if (x < 0) {
-            throw new NoSuchElementException();
-        }
-        Nodo p = root;
+        Nodo n = root;
         for (int i = 0; i < size; i++) {
-            if (p.exponent == x){
-                return p.coefficient;
+            if (n.exponent == x){
+                return n.coefficient;
             }
+            n = n.next;
         }
         throw new NoSuchElementException();
     }
@@ -140,14 +114,16 @@ public class Polynomial {
     // 5. void setCoefficient(int x, int coef): 
     //     establece el valor del coeficiente de grado x al valor ​ coef
     public void setCoefficient(int x, int coef){
-        if (x < 0) {
-            throw new NoSuchElementException();
-        }
         Nodo p = root;
-        for (int i = 0; i < size; i++) {
+        while (p.next != null){
+            System.out.println("p.coeff: "+ p.coefficient);
+            System.out.println("p.exp: "+ p.exponent);
             if (p.exponent == x){
+                System.out.println("Entro al IF stment");
                 p.coefficient = coef;
+                return;
             }
+            p = p.next;
         }
         throw new NoSuchElementException();
     }
@@ -156,8 +132,10 @@ public class Polynomial {
     //     calcula y retorna el valor del polinomio en el punto x.
     public float valueOf(float x) {
         float valued = 0f;
-        for (int i = 0; i < size; i++){
-         valued += Math.pow(root.coefficient,root.exponent);
+        while(root != null){
+         valued += (root.coefficient * (Math.pow( x , root.exponent )));
+         System.out.println(valued);
+         root = root.next;
         }
         return valued;
     }
@@ -165,7 +143,20 @@ public class Polynomial {
     //     retorna true ​ si ​ this ​ es igual a ​ x ​ , y ​ false ​ en caso contrario.
     @Override
     public boolean equals(Object x) {
-        return this == x;
+        Nodo p = root;
+        Polynomial compared;
+        if (x instanceof Polynomial) {
+            compared = (Polynomial) x;
+            while (p != null){
+                if (p.coefficient != compared.root.coefficient || p.exponent != compared.root.exponent){
+                    return false;
+                }
+                p = p.next;
+                compared.root = compared.root.next;
+            }
+        }
+        
+        return true;
     }
 
 
@@ -173,16 +164,24 @@ public class Polynomial {
     //     retorna la representación del ​ Polynomial ​ en forma de ​ String ​ .
     @Override
     public String toString() {
-        String s = "";
-        for (Nodo x = root.next; x != null; x = x.next) {
-            if(x.coefficient > 0) {
-                s = s + " + " +   x.coefficient  + "x^" + x.exponent;
+        StringBuilder sb = new StringBuilder("f(x)=");
+        Nodo p = root;
+        while(p != null) {
+            if(p.coefficient > 0) {
+                sb.append(" + ");
+                sb.append(p.coefficient);
+                sb.append("x^"); 
+                sb.append(p.exponent);
             }
-            else if (x.coefficient < 0) {
-                s = s + " - " + (-x.coefficient) + "x^" + x.exponent;
+            else if (p.coefficient < 0) {
+                sb.append(" - ");
+                sb.append(p.coefficient);
+                sb.append("x^"); 
+                sb.append(p.exponent);
             }
+            p = p.next;
         }
-        return s;
+        return sb.toString();
     }
 
 
--- src/test/java/com/incluit/trainnings/java/ykarqui/polynomial/PolynomialTest.java
+++ src/test/java/com/incluit/trainnings/java/ykarqui/polynomial/PolynomialTest.java
@@ -3,7 +3,6 @@ package com.incluit.trainnings.java.ykarqui.polynomial;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.NoSuchElementException;
 
 import static org.junit.Assert.*;
 
@@ -19,23 +18,110 @@ public class PolynomialTest {
     @Test
     public void testEmptyPolynomSize() {
         pol = new Polynomial();
-        assertEquals(1 , pol.size);
+        assertEquals(1 , pol.size); // pol.size = 1  => {0x^0}
+        assertEquals(0 , pol.getCoefficient(0));
     }
 
     @Test
     public void testEmptyPolynomValuesSize() {
         int coeffs [] = {2, 5, 7};
         pol = new Polynomial(coeffs);
-        assertEquals(3 , pol.size);
+        assertEquals(3 , pol.size);     // 3 == 3
+        assertEquals(coeffs.length , pol.size); // 3 == 3
     }
 
     @Test
-    public void testEmptyPolynomCoefficientValues() {
+    public void testPolynomCoefficientValues() {
         int coeffs [] = {2, 5, 7};
         pol = new Polynomial(coeffs);
         assertEquals(2 , pol.root.coefficient);
         assertEquals(5 , pol.root.next.coefficient);
         assertEquals(7 , pol.root.next.next.coefficient);
+        
+        assertEquals(2 , pol.getCoefficient(1));
+        assertEquals(5 , pol.getCoefficient(2));
+        assertEquals(7 , pol.getCoefficient(3));
+    }
+
+    @Test
+    public void testPolynomPowValues() {
+        int coeffs [] = {2, 5, 7, 10};
+        pol = new Polynomial(coeffs);
+        assertEquals(1 , pol.root.exponent);
+        assertEquals(2 , pol.root.next.exponent);
+        assertEquals(3 , pol.root.next.next.exponent);
+        assertEquals(4 , pol.root.next.next.next.exponent);
+    }
+
+    @Test
+    public void testPolynomSum() {
+        int a [] = {2, 5, 7};
+        int b [] = {4, 7, 3};
+
+        Polynomial pol1 = new Polynomial(a);
+        Polynomial pol2 = new Polynomial(b);
+
+        Polynomial result = pol1.add(pol2);
+
+        assertEquals(6 , result.root.coefficient);
+        assertEquals(12 , result.root.next.coefficient);
+        // assertEquals(10 , result.root.next.next.coefficient);
+    }
+
+    @Test
+    public void testPolynomCoefficientValuesWithFunction() {
+        int coeffs [] = {2, 5, 7};
+        pol = new Polynomial(coeffs);
+        assertEquals(2 , pol.getCoefficient(1));
+        assertEquals(5 , pol.getCoefficient(2));
+    }
+
+    @Test
+    public void testPolynomSetCoefficientValuesWithFunction() {
+        int coeffs [] = {2, 5};
+        pol = new Polynomial(coeffs);
+        pol.setCoefficient(1, 3);
+        assertEquals(3 , pol.getCoefficient(1));
+    }
+
+    @Test
+    public void testToStringNotEmptyPolynom() {
+        int coeffs [] = {7,3,9};
+        Polynomial pol = new Polynomial(coeffs);
+        assertEquals("f(x)= + 7x^1 + 3x^2 + 9x^3", pol.toString());
+    }
+
+    @Test
+    public void testValueOfMethod() {
+        int coeffs [] = {4,3,2};
+        Polynomial pol = new Polynomial(coeffs);
+        
+        // float point1 = 1f;
+        float point2 = 2f;
+        // float expectedPow1 = 9f;
+        float expectedPow2 = 36f;
+        double delta = 0.01;
+        
+        //assertEquals(expectedPow1, pol.valueOf(point1), delta);
+        assertEquals(expectedPow2, pol.valueOf(point2), delta);
+    }
+
+    @Test
+    public void testEqualsPolynoms() {
+        int coeffs [] = {7,3,9};
+        int coeffs2 [] = {7,3,9};
+        Polynomial pol = new Polynomial(coeffs);
+        Polynomial pol2 = new Polynomial(coeffs2);
+        assertEquals(true, pol.equals(pol2));
+    }
+
+    @Test
+    public void testNotEqualsPolynoms() {
+        int coeffs [] = {7,3,9};
+        int coeffs2 [] = {7,3,5};
+        Polynomial pol = new Polynomial(coeffs);
+        Polynomial pol2 = new Polynomial(coeffs2);
+        assertFalse(pol.equals(pol2));
     }
 
 
